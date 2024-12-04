2572b1a061caebbf14cfae8d4758785f262cf649
Luiz Cavani <luizguilhermemacarini@gmail.com>
2024-12-04 08:45:16 -0300

    Adicionados scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 function Home() {
-    return (
-        <body>
-            <h1>Lembre-se</h1>
-            <p>
-                <i>O Aprendizado é uma jornada, não uma corrida!</i>
-            </p>
-        </body>
-    )
+  return (
+    <body>
+      <h1>Lembre-se</h1>
+      <p>
+        <i>O Aprendizado é uma jornada, não uma corrida!</i>
+      </p>
+    </body>
+  );
 }
 
 export default Home;
