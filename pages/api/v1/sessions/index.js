e61bfe9d477152e442fb99891bec2e3ef5ef0b17
Luiz Cavani <luizguilhermemacarini@gmail.com>
2025-11-06 16:46:41 -0300

    feat: create `authorization` model and use it inside `/sessions` controller

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,7 +1,9 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
+import { ForbiddenError } from "infra/errors";
 import authentication from "models/authentication.js";
 import session from "models/session.js";
+import authorization from "models/authorization";
 
 const router = createRouter();
 
@@ -18,6 +20,13 @@ async function postHandler(request, response) {
     userInputValues.email,
     userInputValues.password,
   );
+
+  if (!authorization.can(authenticatedUser, "create:session")) {
+    throw new ForbiddenError({
+      message: "Você não possui permissão para fazer login.",
+      action: "Contate o suporte caso você acredite qeu isto seja um erro.",
+    });
+  }
 
   const newSession = await session.create(authenticatedUser.id);
 
--- tests/integration/api/v1/sessions/delete.test.js
+++ tests/integration/api/v1/sessions/delete.test.js
@@ -65,7 +65,7 @@ describe("DELETE /api/v1/user", () => {
       });
     });
 
-    test("With valid session", async () => {
+    test.only("With valid session", async () => {
       const createdUser = await orchestrator.createUser();
 
       const sessionObject = await orchestrator.createSession(createdUser.id);
